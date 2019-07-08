4e295fa6b85115d417874cba44309db866c27aa0
Robert Fritzen <rfritzen@phantomdev.net>
2019-07-08 17:28:48 -0500

    Remove old EULAs, Redundant Files

--- Advanced FPS Kit/AFPSK_Lite/AI Lite/game/scripts/server/AI/aiArmors.cs
+++
@@ -1,8 +0,0 @@
-//aiArmors.cs
-//Robert Fritzen (Phantom139)
-//(C) 2014, Phantom Games Development
-//Armor Datablocks and definitions for AI
-
-datablock PlayerData(GenericAI : DefaultPlayerData) {
-   shootingDelay = 200;
-};
